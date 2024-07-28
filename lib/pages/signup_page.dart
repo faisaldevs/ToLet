8f7fcd39d2c024738d85cf6753c6d37b76baeaa8
Md Faisal Ahamed <faisalah331@gmail.com>
2024-07-28 13:44:24 +0600

    commit

--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -14,6 +14,7 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
+
   final TextEditingController nameController = TextEditingController();
 
   // @override
